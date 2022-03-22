f6a2ec0c5f44290923443204ac812e42b2cb8b75
Charles Havener <charles.havener@gmail.com>
2022-03-22 11:34:34 -0700

    added command line arguments for area, start_date, and end_date

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 from plots import *
 import pandas as pd
 import os
+import sys, getopt
+
+def get_args(argv):
+    area = ""
+    start_date = "1/1/1900"
+    end_date = "1/1/3000"
+    if len(argv)==0:
+        print(f"main.py -a <area> -s <start_date> -e <end_date>\nmain.py -h <for help>")
+        sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv, "ha:s:e:", ["area=", "start_date=", "end_date="])
+    except getopt.GetoptError:
+        print(f"main.py -a <area> -s <start_date> -e <end_date>\nmain.py -h <for help>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(f"main.py -a <area> -s <start_date> -e <end_date>\n" +
+                  f"e\n" +
+                  f"e")
+        elif opt in ("-a", "--area"):
+            area = arg
+        elif opt in ("-s", "--start_date"):
+            start_date = arg
+        elif opt in ("-e", "--end_date"):
+            end_date = arg
+
+    return area, start_date, end_date
+
+def main(argv):
+    """Generates a series of charts and graphs to present temperature and humidity data
+    """
+
+    area, start_date, end_date = get_args(argv)
+    print(f"Creating charts for {area} for dates in range [{start_date} - {end_date}]")
 
 
-def main():
+
+def _main():
     """Generates a series of charts and graphs to represent temperature and humidity data
     """
 
@@ -16,11 +51,13 @@ def main():
     # Read the excel file and get a list of all its sheets
     xl = pd.ExcelFile(FILE_LOCATION)
     sheets = xl.sheet_names
+    print(sheets)
 
     # Dictionaries to map parameters to strings for file naming
     data_dict = {"TEMP F": "Temp", "RH %": "RH"}
     day_dict = {0: "All Data", 7: "7 Points", 20: "20 Points", 30: "30 Points", 60: "60 Points"}
 
+    '''
     # Loop through each tab to create charts for each functional area
     for group in sheets:
 
@@ -55,6 +92,7 @@ def main():
             for categories,days in [(['DATE'],60), (['DATE', 'Window'],20)]:
                 output_name = f"Images/{group}/Control/{group}-{data_dict[data_name]}-{', '.join(c for c in categories)}-{day_dict[days]}"
                 CreateControlRangeCharts(df, output_name, data_name, categories, days)
+    '''
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
